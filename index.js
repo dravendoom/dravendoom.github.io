bbc4194e0008a8c83a05a82f7b751a6e83e14eef
dravendoom <dravendoom@gmail.com>
2023-02-28 03:37:12 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/newhome', (req, res) => {
+    res.render('newIndex');
+});
+
+
+const server = app.listen(80, '108.167.189.16', () => {
+});
